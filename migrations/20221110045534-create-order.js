9e62f1545af39e6f3e97fd7460054b446be0d1fe
Sony MelliFriady <zoenasun@gmail.com>
2022-11-10 14:49:47 +0700

    edit menjadi promise

--- migrations/20221110045534-create-order.js
+++ migrations/20221110045534-create-order.js
@@ -1,8 +1,8 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Orders', {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Orders', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,7 +31,7 @@ module.exports = {
       }
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Orders');
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Orders');
   }
 };
