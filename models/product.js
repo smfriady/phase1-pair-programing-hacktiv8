cd4948c77b655a2169d930bbf04c8003dc0a605d
Sony MelliFriady <zoenasun@gmail.com>
2022-11-10 15:00:35 +0700

    associate

--- models/product.js
+++ models/product.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Product.hasMany(models.Order);
+      Product.belongsTo(models.Category);
     }
   }
   Product.init({
